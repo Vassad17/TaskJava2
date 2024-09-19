d810bbe3e92db67bd586516c5a97001f3b1f343a
Vasiliy <Ruyk17@gmail.com>
2024-09-19 14:50:51 +0300

    Initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15000;
+
+        int x = 20;
+
+        int bonusMile = amount / x;
+
+        System.out.println ("Бонусные мили:" + bonusMile);
+
+    }
+}
